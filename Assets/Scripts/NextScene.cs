2f4952cf764c1a4ef9f15d95a8528613a76eb17e
Gwendoline <gwendoline.serandour@telecom-sudparis.eu>
2019-11-23 16:13:05 +0100

    ajout changement de scene + prefab joueur

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+    public string nextScene;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
