8ab2c73877f7671fedd203aa38c67a1b87483ec0
Gwendoline <gwendoline.serandour@telecom-sudparis.eu>
2019-11-23 10:35:06 +0100

    Il bouge dans le bon sens maintenant :D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,61 +8,61 @@ public class PlayerController : MonoBehaviour
     public GameObject player; 
     private float deplacementDroit = 0.1f; 
     private float deplacementDiag; 
-    private bool[] UpDownRightLeft= new bool[4]; 
+    private bool[] RightLeftUpDown= new bool[4]; 
     private bool shiftClick = false; 
     private int timer = 0; 
 // Start is called before the first frame update 
     void Start() 
     { 
         deplacementDiag = Mathf.Sqrt((Mathf.Pow(deplacementDroit,2f))/2); 
-        UpDownRightLeft[0] = false; 
-        UpDownRightLeft[1] = false; 
-        UpDownRightLeft[2] = false; 
-        UpDownRightLeft[3] = false; 
+        RightLeftUpDown[0] = false; 
+        RightLeftUpDown[1] = false; 
+        RightLeftUpDown[2] = false; 
+        RightLeftUpDown[3] = false; 
     } 
 // Update is called once per frame 
     void Update() 
     { 
         if (Input.GetKeyDown(KeyCode.UpArrow)) 
         { 
-            UpDownRightLeft[0] = true; 
+            RightLeftUpDown[2] = true; 
         } 
         if (Input.GetKeyDown(KeyCode.DownArrow)) 
         { 
-            UpDownRightLeft[1] = true; 
+            RightLeftUpDown[3] = true; 
         } 
         if (Input.GetKeyDown(KeyCode.RightArrow)) 
         { 
-            UpDownRightLeft[2] = true; 
+            RightLeftUpDown[0] = true; 
         } 
         if (Input.GetKeyDown(KeyCode.LeftArrow)) 
         { 
-            UpDownRightLeft[3] = true; 
+            RightLeftUpDown[1] = true; 
         } 
         if (Input.GetKeyUp(KeyCode.UpArrow)) 
         { 
-            UpDownRightLeft[0] = false; 
+            RightLeftUpDown[2] = false; 
         } 
         if (Input.GetKeyUp(KeyCode.DownArrow)) 
         { 
-            UpDownRightLeft[1] = false; 
+            RightLeftUpDown[3] = false; 
         } 
         if (Input.GetKeyUp(KeyCode.RightArrow)) 
         { 
-            UpDownRightLeft[2] = false; 
+            RightLeftUpDown[0] = false; 
         } 
         if (Input.GetKeyUp(KeyCode.LeftArrow)) 
         { 
-            UpDownRightLeft[3] = false; 
+            RightLeftUpDown[1] = false; 
         } 
-        if (UpDownRightLeft[0]) 
+        if (RightLeftUpDown[0]) 
         { 
-            if (UpDownRightLeft[3]) 
+            if (RightLeftUpDown[3]) 
             { 
                 player.transform.rotation = Quaternion.AngleAxis(-45, new Vector3(0,1,0)); 
                 player.transform.Translate(deplacementDiag, 0, 0); 
             } 
-            else if (UpDownRightLeft[2]) 
+            else if (RightLeftUpDown[2]) 
             { 
                 player.transform.rotation = Quaternion.AngleAxis(45, new Vector3(0,1,0)); 
                 player.transform.Translate(deplacementDiag, 0, 0); 
@@ -73,14 +73,14 @@ public class PlayerController : MonoBehaviour
                 player.transform.Translate(deplacementDroit, 0, 0); 
             } 
         } 
-        if (UpDownRightLeft[1]) 
+        if (RightLeftUpDown[1]) 
         { 
-            if (UpDownRightLeft[3]) 
+            if (RightLeftUpDown[3]) 
             { 
                 player.transform.rotation = Quaternion.AngleAxis(-135, new Vector3(0,1,0)); 
                 player.transform.Translate(deplacementDiag, 0, 0); 
             } 
-            else if (UpDownRightLeft[2]) 
+            else if (RightLeftUpDown[2]) 
             { 
                 player.transform.rotation = Quaternion.AngleAxis(135, new Vector3(0,1,0)); 
                 player.transform.Translate(deplacementDiag, 0, 0); 
@@ -91,12 +91,12 @@ public class PlayerController : MonoBehaviour
                 player.transform.Translate(deplacementDroit, 0, 0); 
             } 
         } 
-        if ((UpDownRightLeft[2]) && (!UpDownRightLeft[1]) && (!UpDownRightLeft[0])) 
+        if ((RightLeftUpDown[2]) && (!RightLeftUpDown[1]) && (!RightLeftUpDown[0])) 
         { 
             player.transform.rotation = Quaternion.AngleAxis(-90, new Vector3(0,1,0)); 
             player.transform.Translate(deplacementDroit, 0, 0); 
         } 
-        if ((UpDownRightLeft[3]) && (!UpDownRightLeft[1]) && (!UpDownRightLeft[0])) 
+        if ((RightLeftUpDown[3]) && (!RightLeftUpDown[1]) && (!RightLeftUpDown[0])) 
         { 
             player.transform.rotation = Quaternion.AngleAxis(90, new Vector3(0,1,0)); 
             player.transform.Translate(deplacementDroit, 0, 0); 
