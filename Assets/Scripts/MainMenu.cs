2a0d25331661094c0c80a322a4929866f7071d6d
Meaux Saint Marc Philippine <philippinemsm@laposte.net>
2019-11-23 16:05:10 +0100

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button play;
+    public Button exit;
+
+    public void Start()
+    {
+        play.onClick.AddListener(TaskOnClickPlay);
+        exit.onClick.AddListener(TaskOnClickExit);
+    }
+
+    void TaskOnClickPlay()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    void TaskOnClickExit()
+    {
+        Application.Quit();
+    }
+}
