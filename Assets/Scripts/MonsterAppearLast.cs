229bfcaab0cab05d5f24d3b1df8ac853294a71fd
Meaux Saint Marc Philippine <philippinemsm@laposte.net>
2019-11-24 12:17:22 +0100

    Le apparait à la fin

---
+++ Assets/Scripts/MonsterAppearLast.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAppearLast : MonoBehaviour
+{
+
+    public GameObject monster;
+    public AudioSource source;
+    public AudioClip clip;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("stop"))
+        {
+            monster.SetActive(true);
+            source.clip = clip;
+            source.Play();
+        }
+    }
+}
